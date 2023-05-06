fb9e4c529b5160577a282a21515a51f92a8e5e39
erenbeyyy <y.eren600@gmail.com>
2023-05-06 10:29:20 +0300

    vize-project-update

--- project/vize-final/vize-final/Program.cs
+++ project/vize-final/vize-final/Program.cs
@@ -1,64 +1,51 @@
 ﻿using System;
 
-namespace NotOrtalamasiHesaplama
+namespace basariNotu
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Kullanıcıya Vize notunu sormak için kullanılır.
-            Console.Write("Vize Notunuzu Giriniz: ");
-
-            // Kullanıcının girdiği Vize notunu Int16 tipine dönüştürür.
-            Int16 vize1 = Convert.ToInt16(Console.ReadLine());
-
-            // Kullanıcının girdiği Vize notunu ekrana yazdırır.
-            Console.WriteLine("Vize Notunuz: " + vize1);
-
-            // Vize notunun yüzde 40'ını hesaplar ve ekrana yazdırır.
-            Console.WriteLine("Vize yüzde notu: " + vize1 * 0.4);
-
-            // Kullanıcıya Final notunu sormak için kullanılır.
-            Console.Write("Final Notunuzu Giriniz: ");
-
-            // Kullanıcının girdiği Final notunu Int16 tipine dönüştürür.
-            Int16 final1 = Convert.ToInt16(Console.ReadLine());
-
-            // Kullanıcının girdiği Final notunu ekrana yazdırır.
-            Console.WriteLine("Final notunuz: " + final1);
-
-            // Final notunun yüzde 60'ını hesaplar ve ekrana yazdırır.
-            Console.WriteLine("Final Yüzde Notunuz: " + final1 * 0.6);
-
-            // Vize notunun yüzde 40'ını hesaplar ve ekrana yazdırır.
-            double vize2 = vize1 * 0.4;
-            Console.WriteLine("Vize yüzdeniz: " + vize2);
-
-            // Final notunun yüzde 60'ını hesaplar ve ekrana yazdırır.
-            double final2 = final1 * 0.6;
-            Console.WriteLine("Final yüzdeniz: " + final2);
-
-            // Vize ve Final notlarının ortalamasını hesaplar.
-            double basariNotu = vize2 + final2;
-
-            // Başarı notunu ekrana yazdırır.
-            Console.WriteLine("Başarı notunuz: " + basariNotu);
-
-            // Başarı notu 60'dan büyükse "Geçti", 50'den büyükse "Şartlı Geçti", aksi halde "Kaldı" yazdırır.
-            if (basariNotu >= 60)
+            try
             {
-                Console.WriteLine("Geçti");
+                Console.Write("Vize Notunuzu Giriniz (0-100 aralığında): ");
+                int vize = Convert.ToInt32(Console.ReadLine());
+
+                if (vize < 0 || vize > 100)
+                {
+                    throw new Exception("Girilen değer 0-100 aralığında değil!");
+                }
+
+                Console.Write("Final Notunuzu Giriniz (0-100 aralığında): ");
+                int final = Convert.ToInt32(Console.ReadLine());
+
+                if (final < 0 || final > 100)
+                {
+                    throw new Exception("Girilen değer 0-100 aralığında değil!");
+                }
+
+                double ortalama = (vize * 0.4) + (final * 0.6);
+                Console.WriteLine("Ortalamanız: " + ortalama);
+
+                if (ortalama >= 60)
+                {
+                    Console.WriteLine("Tebrikler, dersi başarıyla geçtiniz!");
+                }
+                else if (ortalama >= 50)
+                {
+                    Console.WriteLine("Dersi şartlı olarak geçtiniz.");
+                }
+                else
+                {
+                    Console.WriteLine("Dersi maalesef geçemediniz.");
+                }
             }
-            else if (basariNotu >= 50)
+            catch (Exception ex)
             {
-                Console.WriteLine("Şartlı Geçti");
-            }
-            else
-            {
-                Console.WriteLine("Kaldı");
+                Console.WriteLine("Hata: " + ex.Message);
             }
 
-            // Kullanıcının bir tuşa basmasını bekler ve programı sonlandırır.
+            Console.WriteLine("Programı kapatmak için bir tuşa basın...");
             Console.ReadKey();
         }
     }
